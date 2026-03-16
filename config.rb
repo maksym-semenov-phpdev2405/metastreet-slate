cbabf6ff59f311d953499069374eb21bcab47f12
maksym-semenov-phpdev2405 <maksym.semenov@everlution.sk>
2026-03-16 18:43:03 +0100

    require 'uglifier'

--- config.rb
+++ config.rb
@@ -41,6 +41,7 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
+  require 'uglifier'
   # We do want to hash woff and woff2 as there's a bug where woff2 will use
   # woff asset hash which breaks things. Trying to use a combination of ignore and
   # rewrite_ignore does not work as it conflicts weirdly with relative_assets. Disabling
